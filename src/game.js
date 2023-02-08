1884ffe0d9dc69c25c59055f46ecf7cfa6f56a2d
Juan José Gómez A <jjgomeza22@gmail.com>
2023-02-07 22:34:32 -0500

    Print bombs in canvas

--- src/game.js
+++ src/game.js
@@ -2,8 +2,24 @@ const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
 const startGame = () => {
-    game.fillRect(0,0, 100,100);
-    game.clearRect(50,50, 100,100);
+    let canvasSize;
+
+    canvasSize = window.innerHeight * 0.8;
+    if (window.innerHeight > window.innerWidth){
+        canvasSize = window.innerWidth * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    const elementSize = canvasSize / 10;
+    console.log(elementSize);
+    game.font = elementSize.toString() + 'px Verdana';
+    game.textAlign = 'end';
+
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], elementSize* i, elementSize);
+    }
 };
 
 window.addEventListener('load', startGame);
